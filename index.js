a40a763d2f70f94713515a9271173d2b8b58a6e6
Mitchell Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2020-02-05 21:22:54 +1100

    Initial commit of app files

---
+++ index.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const dbPassword = process.env.DB_ADMIN_PASS;
+const dbUrl = `mongodb+srv://admin:${dbPassword}@cluster0-vl3pn.mongodb.net/
+               test?retryWrites=true&w=majority`
+               
+const app = express();
+
+const PORT = 9000;
+
+const register = require('./routes/register');
+const login = require('./routes/login');
+const postNotes = require('./routes/postNotes');
+const fetchNotes = require('./routes/fetchNotes');
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+app.use(cors());
+
+
+// IIFE to check server connection to database
+(function checkAtlasDbConnection () {
+  MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db)=> {
+    if (err) throw err;
+    console.log('Connection established with MongoDB Atlas');
+  })
+}) ();
+
+app.use('/register', register);
+app.use('/login', login);
+app.use('/postNotes', postNotes);
+app.use('/fetchNotes', fetchNotes);
+
+app.listen(PORT, () => console.log(
+  `Server started on http://localhost:${PORT}`
+));
---
+++ routes/utilities.js
@@ -0,0 +1,84 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbPassword = process.env.DB_ADMIN_PASS;
+const dbUrl = `mongodb+srv://admin:${dbPassword}@cluster0-vl3pn.mongodb.net/
+               test?retryWrites=true&w=majority`
+const jwt = require('jsonwebtoken');
+
+const fetchUsers = () => {
+  return new Promise((resolve) => {
+    MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db) => {
+      if (err) throw err; 
+      const dbo = db.db('mydb');
+      dbo.collection('customers').find({}).toArray((err, res) => {
+        if (err) throw err;
+        resolve(res);
+        db.close();
+      })
+    })
+  })
+};
+
+const addNewUser = (newUserObj) => {
+  return new Promise((resolve)=> {
+    MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('mydb');
+      dbo.collection('customers').insertOne(newUserObj, (err, res) => {
+        if (err) throw err;
+        console.log('1 new user added successfully to DB');
+        resolve();
+        db.close();
+      })
+    })
+  })
+}
+
+const verifyUser = (req) => {
+  return new Promise((resolve)=> {
+    if (req.body.token) {
+      
+      jwt.verify(req.body.token, process.env.SECRET_OR_KEY, (err, validToken)=> {
+        if(validToken) resolve(validToken.userEmail);
+      });
+    } else {
+      resolve(false);
+    }
+  })
+}
+
+const fetchNotes = (email) => {
+  return new Promise((resolve)=> {
+    MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db)=> {
+      if (err) throw err;
+      const dbo = db.db('mydb');
+      dbo.collection('customers').find({email: email}).toArray((err, document)=> {
+        if(err) throw err;
+        if (document[0]) resolve(document[0].notes);
+        db.close();
+      })
+    })
+  })
+}
+
+const postNotes = (email, notes) => {
+  return new Promise((resolve)=> {
+    MongoClient.connect(dbUrl, {useNewUrlParser: true}, (err, db)=> {
+      if (err) throw err;
+      const dbo = db.db('mydb');
+      dbo.collection('customers').updateOne({email: email}, {$set: {notes: notes}}, (err, result)=> {
+        if(err) throw err;
+        console.log(`updated notes for ${email}`);
+        resolve(true);
+        db.close();
+      })
+    })
+  })
+}
+
+module.exports = {
+  fetchUsers: fetchUsers,
+  addNewUser: addNewUser,
+  verifyUser: verifyUser,
+  fetchNotes: fetchNotes,
+  postNotes: postNotes,
+}
